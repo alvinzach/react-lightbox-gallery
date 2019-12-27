9d43c43efaa60e903e36697a23bf12a4830abdf3
alvinzach <alvinzach@gmail.com>
2019-12-27 23:49:38 +0530

    initial commit

--- example/src/App.js
+++ example/src/App.js
@@ -3,11 +3,59 @@ import React, { Component } from 'react'
 import ExampleComponent from 'react-lightbox-gallery'
 
 export default class App extends Component {
-  render () {
+  render() {
+    var images = [
+      {
+        src: "https://images.unsplash.com/photo-1577279549270-b9e297533cdd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1534&q=80",
+        desc: 'Person wearing shoes',
+        sub: 'Gift Habeshaw'
+      },
+      {
+        src: "https://images.unsplash.com/photo-1577277625082-36df4915ebeb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+        desc: 'Blonde woman wearing sunglasses smiling at the camera ',
+        sub: 'Dmitriy Frantsev'
+      },
+      {
+        src: "https://images.unsplash.com/photo-1577278689329-1914b6814d58?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1140&q=80",
+        desc: 'Man and woman passing by store',
+        sub: 'Harry Cunningham'
+      },
+      {
+        src: "https://images.unsplash.com/photo-1577177205947-4078546be974?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1534&q=80",
+        desc: 'Jaipur , Rajasthan India',
+        sub: 'Liam Baldock'
+      },
+      {
+        src: "https://images.unsplash.com/photo-1437326300822-01d8f13c024f?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+        sub: 'Verne Ho'
+      },
+      {
+        src: "https://images.unsplash.com/photo-1549468057-5b7fa1a41d7a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80",
+        desc: 'Rann of kutch , India',
+        sub: 'Hari Nandakumar'
+      },
+      {
+        src: "https://images.unsplash.com/photo-1521859727374-9b8e0ef8c0c3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
+        sub: 'Jyotirmoy Gupta'
+      },
+      {
+        src: "https://images.unsplash.com/photo-1577201404214-b725e3212fc6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80",
+        sub: 'Tom & Sini'
+      }
+    ]
+    var settings = {
+      columnCount:{
+        default:5,
+        mobile:3,
+        tab:4
+      },
+      mode: 'dark'
+    }
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <ExampleComponent images={images} settings={settings} />
       </div>
     )
   }
 }
+
--- src/index.js
+++ src/index.js
@@ -1,22 +1,141 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-
 import styles from './styles.css'
 
 export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
+  // intial state 
+  state = {
+    length: 0,
+    lightroomactive: false,
+    activeindex: 0,
+    path: '',
+    desc: '',
+    sub: ''
+  }
+  // varibles to store props
+  size = 4
+  mode = 'light'
+  componentDidMount() {
+    this.setState({
+      length: this.props.images.length,
+    })
+  }
+  // controlling lightbox activation
+  openlightroom = (e) => {
+    this.setState({
+      lightroomactive: true
+    })
+    this.getcontent(e.target.getAttribute('data-index'))
   }
 
-  render() {
-    const {
-      text
-    } = this.props
+  closelightroom = () => {
+    this.setState({
+      lightroomactive: false
+    })
+  }
+  //lightbox controls
+  moveright = () => {
+    var id = parseInt(this.state.activeindex, 10)
+    if (id == this.state.length - 1)
+      this.setState({
+        lightroomactive: false
+      })
+    else
+      this.getcontent(++id)
+  }
+  moveleft = () => {
+    var id = parseInt(this.state.activeindex, 10)
+    if (id == 0)
+      this.setState({
+        lightroomactive: false
+      })
+    else
+      this.getcontent(--id)
+  }
+  getcontent = (id) => {
+    this.setState(
+      {
+        activeindex: id,
+        path: this.props.images[id].src,
+        sub: this.props.images[id].sub,
+        desc: this.props.images[id].desc
+      }
+    )
+  }
 
+  calculateStyles = () => {
+    var columncount = 4,
+      lightroomBackground = 'rgba(255,255,255,0.95)',
+      textColor = '#000'
+    if (this.props.settings) {
+      if (this.props.settings.columnCount) {
+        if (window.outerWidth <= 600)
+          columncount = this.props.settings.columnCount.mobile ? this.props.settings.columnCount.mobile : 2
+        else if (window.outerWidth <= 800)
+          columncount = this.props.settings.columnCount.tab ? this.props.settings.columnCount.tab : 3
+        else
+          columncount = this.props.settings.columnCount.default ? this.props.settings.columnCount.default : 5
+      }
+      lightroomBackground=this.props.settings.mode == 'light' ? 'rgba(255,255,255,0.95)' : 'rgba(0,0,0,0.95)'
+      textColor=this.props.settings.mode == 'light' ?'#000' : '#fff'
+    }
+        return {
+          row: {
+            columnCount: columncount
+          },
+          lightroom: {
+            backgroundColor:lightroomBackground,
+            color:textColor
+          }
+        }
+    }
+  
+  render() {
+    const runtimeStyles = this.calculateStyles()
+    document.onkeydown = (e) => {
+      if (e.keyCode === 39)
+        this.moveright()
+      if (e.keyCode === 37)
+        this.moveleft()
+    }
     return (
-      <div className={styles.test}>
-        Example Component: {text}
+      <div>
+
+        <div className={styles.row} style={runtimeStyles.row}>
+
+          {this.props.images.map((img, i) =>
+            <div className={styles.column} >
+              <img data-index={i} src={img.src} key={i} onClick={this.openlightroom} />
+            </div>
+          )}
+
+        </div>
+
+        <div className={styles.lightroom} style={{
+          display: this.state.lightroomactive ? 'block' : 'none',
+          ...runtimeStyles.lightroom
+        }} >
+
+
+          <img className={styles.closebutton} src={require('./img/closebutton.svg')} onClick={this.closelightroom} />
+          <div className={styles.lightroomcontent} >
+            <img className={styles.lightroomimg} src={this.state.path} style={{ maxWidth: "100%" }} />
+          </div>
+          <div className={styles.lightroomdesc}>
+            <h1>{this.state.name}</h1>
+            <p className={styles.desc}>{this.state.desc}</p>
+            <p className={styles.sub}>{this.state.sub}</p>
+          </div>
+
+          <a className={styles.carouselcontrolprev} role="button" tabindex="0" onClick={this.moveleft}>
+            <img src={require('./img/left.svg')} />
+          </a>
+          <a class={styles.carouselcontrolnext} role="button" tabindex="0" onClick={this.moveright}>
+            <img src={require('./img/right.svg')} />
+          </a>
+        </div>
+
       </div>
+
     )
   }
 }
